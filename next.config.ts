457216ef94c1396ec7b7fcd0c18228c08a201bd3
Nikhil <nikhilmathew863@gmail.com>
2025-12-29 09:48:57 +0000

    replace the first image with https://d.imgvision.net/nikz/WhatsApp_Image

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'd.imgvision.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
